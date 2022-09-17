a2302077746fa024ce1478085841cd6d18d4c4fa
Gaurav Pandey <yednapg@gmail.com>
2022-09-17 19:39:52 +0530

    added addStudent

--- frontend/pages/addStudent/[studentclass].tsx
+++ frontend/pages/addStudent/[studentclass].tsx
@@ -9,25 +9,41 @@ import { database } from '../../config.keys'
 import styles from '../styles/Home.module.css'
 
 
-const Selectclass:NextPage=()=>{
-    const router =useRouter();
-    let {studentclass}=router.query;
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+// import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { TextField } from '@mui/material'
+// import Search from '../components/search'
 
-    const [studnet,setStudent]=useState<any>({
-        name:"",
-        dateofbirth:"",
-        admissionNumber:"",
-        presentDates:[],
-        absentDates:[]
+
+const Selectclass: NextPage = () => {
+    const router = useRouter();
+    let { studentclass } = router.query;
+
+    const [studnet, setStudent] = useState<any>({
+        name: "",
+        dateofbirth: "",
+        admissionNumber: "",
+        presentDates: [],
+        absentDates: []
     })
 
-    const {name,dateofbirth,admissionNumber,presentDates,absentDates}=studnet;
+    const { name, dateofbirth, admissionNumber} = studnet;
 
     // class=string(class)
     // studentclass=String(studentclass) || " "
-    const onSubmit=()=>{
-        
-    
+    const handleSubmit = () => {
+
+
     }
 
     // useEffect(()=>{
@@ -38,9 +54,85 @@ const Selectclass:NextPage=()=>{
 
     return (
         <>
-       profile
-        
-      <input type="text" name="" id="" />
+            {/* <Search /> */}
+            <Container component="main" maxWidth="xs">
+                <CssBaseline />
+
+                <Box
+                    sx={{
+                        marginTop: 8,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+
+
+                    }}
+                >
+                    <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                        <LockOutlinedIcon />
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
+                        Sign up
+                    </Typography>
+                    <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+                        <Grid container spacing={2}>
+                            <Grid item xs={12}>
+                                <TextField
+                                    autoComplete="given-name"
+                                    // name="firstName"
+                                    required
+                                    fullWidth
+                                    id="firstName"
+                                    label="Name"
+                                    autoFocus
+                                    name="name"
+                                    value={name}
+                                // onChange={(e) => setUser({ ...user, [e.target.name]: e.target.value })}
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    required
+                                    fullWidth
+                                    id="dob"
+                                    label="Date of Birth"
+                                    name="dob"
+                                // value={email}
+                                // onChange={(e) => setUser({ ...user, [e.target.name]: e.target.value })}
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    required
+                                    fullWidth
+                                    name="admissionNumber"
+                                    label="Admin No."
+                                    type="password"
+                                    id="admissionNumber"
+                                    autoComplete="admissionNumber"
+                                // value={password}
+                                // onChange={(e) => setUser({ ...user, [e.target.name]: e.target.value })}
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                {/* <FormControlLabel
+                  control={<Checkbox value="allowExtraEmails" color="primary" />}
+                  label="I want to receive inspiration, marketing promotions and updates via email."
+                /> */}
+                            </Grid>
+                        </Grid>
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            sx={{ mt: 3, mb: 2 }}
+                        >
+                            Add
+                        </Button>
+                    </Box>
+                </Box>
+                {/* <Copyright sx={{ mt: 5 }} /> */}
+            </Container>
         </>
     )
 }
